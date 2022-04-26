ad850713d5109b17361280ba2df17423d11adc7d
Gabriel Ricardo <gabrielricardo.silva17@gmail.com>
2022-04-26 19:35:33 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+clicou = () =>{
+    let texto = document.querySelector('#lmao')
+    texto.innerHTML = `
+    Clicou 
+    `
+}
+
+passouMouse = () =>{
+    let texto = document.querySelector('#lmao')
+    texto.innerHTML =`
+    Passou o Mouse 
+    `
+}
+tirouMouse = () =>{
+    let texto = document.querySelector('#lmao')
+    texto.innerHTML =`
+    Tirou o Mouse 
+    `
+}
